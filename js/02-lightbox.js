cff189f2e828c518524ddc2b585e7f925b3d83af
VladislavGuliy <vladgulij777@gmail.com>
2023-05-30 20:22:42 +0300

    Рішення завдання №2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const listGalleryEl = document.querySelector(".gallery");
+
+const elements = createGalleryImages(galleryItems);
+
+listGalleryEl.insertAdjacentHTML("beforeend", elements);
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`;
+    })
+    .join("");
+}
 
 console.log(galleryItems);
